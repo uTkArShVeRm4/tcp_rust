c07ee060e0e94e5e15b6989eb35f400430185c97
uTkArShVeRm4 <theutkarsh.verma00@gmail.com>
2024-02-26 23:11:11 +0530

    Minor changes

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() -> io::Result<()> {
         let nbytes = nic.recv(&mut buf[..])?;
         let _eth_flags: u16 = u16::from_be_bytes([buf[0], buf[1]]);
         let eth_proto: u16 = u16::from_be_bytes([buf[2], buf[3]]);
-
         if eth_proto != 0x0800 {
             // Not an IPv4 protocol
             continue;
@@ -24,17 +23,20 @@ fn main() -> io::Result<()> {
                     // Not a tcp protocol
                     continue;
                 }
-                eprintln!(
-                    "{} -> {}, {}b of protocol {}",
-                    src,
-                    dst,
-                    p.payload_len().unwrap(),
-                    proto.0
-                );
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + p.slice().len()..]) {
+                    Ok(p) => {
+                        eprintln!(
+                            "{} -> {}, {}b of tcp to port {}",
+                            src,
+                            dst,
+                            p.slice().len(),
+                            p.destination_port(),
+                        );
+                    }
+                    Err(_e) => eprintln!("Ignoring weird tcp packet"),
+                }
             }
-            Err(_e) => {
-                eprintln!("Ignoring this packet");
-            }
+            Err(_e) => eprintln!("Ignoring this packet"),
         }
     }
 }
