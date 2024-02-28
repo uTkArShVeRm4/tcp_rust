35f6391ad1efc7553cd28ab3d46bb3be955fe39b
uTkArShVeRm4 <theutkarsh.verma00@gmail.com>
2024-02-28 23:33:20 +0530

    Please work

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,27 @@
-use std::collections::HashMap;
-use std::io;
-use std::net::Ipv4Addr;
-
-mod tcp;
-
-#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
-struct Quad {
-    src: (Ipv4Addr, u16),
-    dst: (Ipv4Addr, u16),
-}
+use std::io::prelude::*;
+use std::{io, thread};
 
 fn main() -> io::Result<()> {
-    let mut connections: HashMap<Quad, tcp::State> = Default::default();
-    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
-    let mut buf = [0u8; 1504];
-
-    loop {
-        let nbytes = nic.recv(&mut buf[..])?;
-        let _eth_flags: u16 = u16::from_be_bytes([buf[0], buf[1]]);
-        let eth_proto: u16 = u16::from_be_bytes([buf[2], buf[3]]);
-        if eth_proto != 0x0800 {
-            // Not an IPv4 protocol
-            continue;
-        }
-
-        match etherparse::Ipv4Slice::from_slice(&buf[4..nbytes]) {
-            Ok(iph) => {
-                let iph = iph.header();
-                let src = iph.source_addr();
-                let dst = iph.destination_addr();
-                let proto = iph.protocol();
-                if proto.0 != 0x06 {
-                    // Not a tcp protocol
-                    continue;
-                }
-                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + iph.slice().len()..nbytes]) {
-                    Ok(tcph) => {
-                        let datai = 4 + iph.slice().len() + tcph.slice().len();
-                        connections
-                            .entry(Quad {
-                                src: (src, tcph.source_port()),
-                                dst: (dst, tcph.destination_port()),
-                            })
-                            .or_default()
-                            .on_packet(iph, tcph, &buf[datai..nbytes]);
-                    }
-                    Err(_e) => eprintln!("Ignoring weird tcp packet"),
+    let mut i = crate::Interface::new()?;
+    eprintln!("created interface");
+    let mut listener = i.bind(8000)?;
+    while let Ok(mut stream) = listener.accept() {
+        eprintln!("got connection!");
+        thread::spawn(move || {
+            stream.write(b"hello from rust-tcp!\n").unwrap();
+            stream.shutdown(std::net::Shutdown::Write).unwrap();
+            loop {
+                let mut buf = [0; 512];
+                let n = stream.read(&mut buf[..]).unwrap();
+                eprintln!("read {}b of data", n);
+                if n == 0 {
+                    eprintln!("no more data!");
+                    break;
+                } else {
+                    println!("{}", std::str::from_utf8(&buf[..n]).unwrap());
                 }
             }
-            Err(_e) => eprintln!("Ignoring this packet"),
-        }
+        });
     }
+    Ok(())
 }
